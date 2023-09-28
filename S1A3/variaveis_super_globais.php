36723829b4d45763abdd5aa9a00faa06003ad483
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-09-28 11:36:20 -0300

    Update variaveis_super_globais.php

--- S1A3/variaveis_super_globais.php
+++ S1A3/variaveis_super_globais.php
@@ -1,8 +1,18 @@
-<?php 
-
+<?php
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Origin: *");
 
-echo json_encode(['message' => $_SERVER['REQUEST_METHOD']]);
+$method = $_SERVER['REQUEST_METHOD'];
 
-?>
+if ($method === 'GET') {
+    $text = file_get_contents('database.txt');
+    $data =  json_decode($text);
+
+    echo json_encode(['products' => $data]);
+    
+} elseif ($method === "POST") {
+    $body = file_get_contents("php://input");
+    $data = json_decode($body);
+
+    echo json_encode(['message' => 'Recebi um post']);
+}
